1d12579455a262fa876ab6d33b522ab566d60f79
JON STILLSON <JCSTILLSON77@GMAIL.COM>
2023-04-22 02:28:33 -0400

    more js

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const usersName = promt("What is your name?");
 console.log(usersName);
 
 document.write("Hi " + usersName + " welcome to my page!");
+
+if (usersName == "Jonathon"){
+    document.write("Hey bossman");
+} else if (usersName == "Kassie"){
+    document.write("Hey teach");
+} else {
+    document.write("Glad to have you here!");
+}
