2c30110d51686a22aff9f108a7ccbc08ca00ac68
JON STILLSON <JCSTILLSON77@GMAIL.COM>
2023-04-22 02:49:24 -0400

    addding logic section

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ console.log(usersName);
 
 document.write("Hi " + usersName + " welcome to my page!");
 
-//if (usersName == "Jonathon"){
-//    document.write("Hey bossman");
-//} else if (usersName == "Kassie"){
-//    document.write("Hey teach");
-//} else {
-  //  document.write("Glad to have you here!");
-//}
+if (usersName == "Jonathon"){
+  document.write("Hey bossman");
+} else if (usersName == "Kassie"){
+  document.write("Hey teach");
+} else {
+  document.write("Glad to have you here!");
+}
